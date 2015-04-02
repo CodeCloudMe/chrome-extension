90dafabdf5eadb00c4ae104cf4e95c1ebbece80a
Chris Williams <Medicinebottle@Chriss-MacBook-Pro.local>
2015-04-02 12:37:33 +0900

    added backend

---
+++ cloud/models/btc/bfChart.php
@@ -0,0 +1,37 @@
+<?php
+
+	
+	require_once($_SERVER['DOCUMENT_ROOT'].'cloud/models/main/index.php');
+	
+
+	ini_set('display_errors',1);  error_reporting(E_ALL); 
+	
+
+	$resp = getData();
+
+
+	echo(json_encode($resp));
+
+
+
+
+	//prop
+
+	function getData(){
+	
+
+		
+		
+		$resp = dbMassData("SELECT * FROM bitfinix ORDER BY timestamp DESC");
+		$resp1 = dbMassData("SELECT * FROM bitstampPrices ORDER BY timestamp DESC");
+		$resp2 = dbMassData("SELECT * FROM Korbit ORDER BY timestamp DESC");
+		$allResp = array();
+		$allResp[0] =  $resp;
+		$allResp[1]= $resp1;
+		$allResp[2]= $resp2;
+
+		return $allResp;
+	}
+
+
+?>
---
+++ cloud/models/main/settings.php
@@ -0,0 +1,11 @@
+<?php
+//your username here for MYSQL
+$username= "adminaVTyFhQ";
+
+//your password here for MYSQL
+$dbPassword = "E38AbVBN1MWg";
+
+//keep database as BTC
+$database= "btcrecords";
+
+?>
